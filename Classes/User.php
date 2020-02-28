2239e576ab4a1d41b48abe653c56c6c595a5a22b
kdenno <kabuyedennis3@gmail.com>
2020-02-28 14:38:41 +0300

    check if user is logged in

--- Classes/User.php
+++ Classes/User.php
@@ -2,11 +2,24 @@
 class User
 {
   private $_db, $_queries, $_userdata, $_sessionName;
+  public $isLoggedIn = false;
   public function __construct($user = null)
   {
     $this->_db = DB::getDb();
     $this->_queries = new Queries($this->_db);
     $this->_sessionName = Config::get('session/session_name');
+    if (!$user) {
+      if (Session::exists($this->_sessionName)) {
+        $userid = Session::get($this->_sessionName);
+        if ($this->find($userid)) {
+          $this->isLoggedIn = true;
+        } else {
+        }
+      }
+    } else {
+      // user id is defined, find the user
+      $this->find($user);
+    }
   }
   public function create($fields = array())
   {
@@ -40,8 +53,12 @@ class User
     }
     return false;
   }
-  private function userdata()
+  public function userdata()
   {
     return $this->_userdata;
+  }
+  public function isLoggedIn()
+  {
+    return $this->isLoggedIn;
   }
 }
